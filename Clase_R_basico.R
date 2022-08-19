f4d9e53e3d88dd26db97e77e3004cb6353da15ed
alrier <you@example.com>
2022-08-18 20:33:47 -0500

    1

--- Clase_R_basico.R
+++ Clase_R_basico.R
@@ -1,5 +1,3 @@
-:)
-
 pkg <- function(pkg){
   new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
   if (length(new.pkg)) 
@@ -30,6 +28,8 @@ archivo <- WideReleasesCount
 #VECTORES----------------------------------------
 a <- c(2,4,1,8)
 b <- c(2,2,2,2)
+c=c("perro","casa", 3)
+d=c(2,45,6,7)
 
 #Puedo poseer diferentes tipos de vectores
 vector_double <-c(1, 2.5, 4.5, 25)
@@ -41,12 +41,19 @@ vector_character <-c("Hola", "Mundo!", "4343434", "ssfkjdfdkjflkshfklj")
 
 #Length nos ayuda a conocer la longitud de cualquier vector. 
 length(vector_logical)
+f=c("mi mundo es bonito")
+length(f)
 length(vector_double)
 length(archivo)
 
 #Operaciones Vectorizadas-------------------------------------------------------------
-c<-a+b
-c<-a/b
+u=a+b
+print(u)
+u
+view(u) 
+l=c(1,2,3) 
+P=c(1,2)
+l+f
 #reciclaje de elementos en los vectores. 
 
 '''supongamos que tenemos dos vectores a y b'''
@@ -69,10 +76,11 @@ ncol = es el número de columnas.
 byrow = es un valor lógico. Si es TRUE el vector que pasamos será ordenado por filas.
 dimnames = nombres asignado a filas y columnas.'''
 #Seguidamente se muestra un ejemplo de creación de una matriz:
-matriz <- matrix(1:12, nrow = 4)
+matriz=matrix(1:12, nrow = 4)
 matriz
+casa=matrix(1:24, ncol=6)
 #ejemplo de una matriz usando los argumentos previamente aprendidos. 
-automoviles <- matrix(
+automoviles = matrix(
   1:12,
   nrow = 4, #numero de filas
   byrow = TRUE, #ordenado por filas.
@@ -82,13 +90,11 @@ automoviles <- matrix(
   )
 )
 
-automoviles <- matrix(
-  1:12,
-  nrow = 4, 
-  byrow = TRUE,
-  dimnames = list(
-    c("Blanco", "Rojo", "Negro", "Gris"),
-    c("Toyota", "Audi", "Nissan")
+automoviles=matrix(
+  1:60,
+  nrow = 6,
+  ncol = 10,
+  byrow = TRUE
   )
 )
 
@@ -97,6 +103,7 @@ automoviles <- matrix(
 v1 <- c(1, 2, 3)
 v2 <- c(4, 5, 6)
 m1 <- cbind(v1, v2)
+m2 <- rbind(v1, v2)
 m1
 
 nombres <- c("Pedro","Maria","Juan")
@@ -108,6 +115,12 @@ v1 <- c(1, 2, 3)
 v2 <- c(4, 5, 6)
 m1 <- rbind(v1, v2)
 m1
+
+Tarea. 
+
+1.Generar cada uno 6 vectores diferentes. 
+2.Unirlos en 3 tablas compuestas en ordenes diferentes. 
+3.Crear 4 ejemplos de matrices diferentes. 
 
 #Listas-------------------------------------------------------------------
 lista <- list(1:3, "Ruben", pi, list(c(-1, -2), -5))
