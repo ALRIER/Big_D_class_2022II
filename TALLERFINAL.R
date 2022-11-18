4cc3eef8ebaa48b22cf2bbbe822f013062870c2c
juanmeneses90 <110642428+juanmeneses90@users.noreply.github.com>
2022-11-17 19:02:26 -0500

    Update TALLERFINAL.R
    1

--- TALLERFINAL.R
+++ TALLERFINAL.R
@@ -18,19 +18,83 @@ packages <- c("tidyverse","raster","sf","ggspatial","cluster","factoextra",
 
 pkg(packages)
 
-setup_twitter_oauth("6gMch1lkCfn3W5PZho3X4jh8W",#api key
-                    "eBosfeWmQVBLQ87WiVMaRXousYRdyOkyhLkvCUzw7ioz0EMCWY",#api secret key
-                    "284827529-UENNMA2jVHCRBYwcddd6obAAZvaJ0hUVSapYYmwZ",#acces token
-                    "sb1fgjDG9CSugsU5qsWJWkBOvP91FxJmcm7hKCyajrndT")#acces token secret
+setup_twitter_oauth("6gMch1lkCfn3W5PZho3X4jh8W",
+                    "eBosfeWmQVBLQ87WiVMaRXousYRdyOkyhLkvCUzw7ioz0EMCWY",
+                    "284827529-UENNMA2jVHCRBYwcddd6obAAZvaJ0hUVSapYYmwZ",
+                    "sb1fgjDG9CSugsU5qsWJWkBOvP91FxJmcm7hKCyajrndT")
 
-a<- searchTwitter("cerveza", n=500)
-a[1:100]
+a<- searchTwitter("cerveza", n=300)
+a[1:300]
 cerveza<-twListToDF(a)
+b<- cerveza %>% filter(isRetweet=="FALSE")
+te<-b$text
 
-create_token(app="Clase_big_data",
-             consumer_key = "6gMch1lkCfn3W5PZho3X4jh8W",#api key
-             consumer_secret = "eBosfeWmQVBLQ87WiVMaRXousYRdyOkyhLkvCUzw7ioz0EMCWY",#api secret key
-             access_token ="284827529-UENNMA2jVHCRBYwcddd6obAAZvaJ0hUVSapYYmwZ",#acces token
-             access_secret ="sb1fgjDG9CSugsU5qsWJWkBOvP91FxJmcm7hKCyajrndT")#acces token secret
+tokens<-tokens(te,what = "word",remove_punct = TRUE,remove_symbols =TRUE,remove_numbers =TRUE,remove_url =TRUE,remove_separators =TRUE,split_hyphens =TRUE)
+text <- gsub("@\\w+", "", tokens)
+text <- gsub("https?://.+", "", text)
+text <- gsub("\\d+\\w*\\d*", "", text)
+text <- gsub("#\\w+", "", text)
+text <- gsub("[^\x01-\x7F]", "", text)
+text <- gsub("[[:punct:]]", " ", text)
 
-te<-Perfiles_Mkt$`Descripcion Vacante`
+text <- gsub("\n", " ", text)
+text <- gsub("^\\s+", "", text)
+text <- gsub("\\s+$", "", text)
+text <- gsub("[ |\t]+", " ", text)
+
+x = gsub("rt", "", text)
+
+x = gsub("@\\w+", "", x)
+
+x = gsub("[[:punct:]]", "", x)
+
+x = gsub("[[:digit:]]", "", x)
+
+x = gsub("http\\w+", "", x)
+
+x = gsub("[ |\t]{2,}", "", x)
+
+x = gsub("^ ", "", x)
+
+x = gsub(" $", "", x)
+
+a<-gsub("[^\x01-\x7F]", "", x)
+
+discurso <- tolower(a)
+
+discurso <- removeWords(discurso, words = stopwords("spanish"))
+
+discurso <- removeWords(discurso, words = c("hp2pdfmnfa","@iandresrm","va","https://","@",":",
+                                            "…","t.co/","👁","@christi11079874","📢","jajajajjaja",
+                                            "“","n","cad…","@lafm","plat…",
+                                            "…", "indra","dos","día","🇨🇴",
+                                            "de","la","aos","el","y","las","nes",
+                                            "ser"))
+
+discurso <- removePunctuation(discurso)
+
+discurso <- removeNumbers(discurso)
+
+discurso1 <- Corpus(VectorSource(discurso)) 
+
+letras<- TermDocumentMatrix(discurso1)
+letrasmatrix <- as.matrix(letras) 
+
+vector <- rowSums(letrasmatrix) 
+Vectorr<- sort(vector, decreasing = T)
+
+view(Vectorr)
+Vectorr[1:20]
+
+dataletras <- data.frame(word= names(Vectorr),freq=Vectorr)  
+
+barplot(dataletras[1:10,]$freq, las = 2, names.arg = dataletras[1:10,]$word, 
+        col = brewer.pal(n = 8, name = "Set3"), main ="Pabalras mas frecuentes",
+        ylab = "Frecuencia de palabras") 
+
+dataletras[1:30, ] %>%
+   ggplot(aes(word, freq)) +
+   geom_bar(stat = "identity", color = "black", fill = "#87CEFA") +
+   geom_text(aes(hjust = 1.3, label = freq))+ 
+   coord_flip() + 
+   labs(title = "Diez palabras más frecuentes",  x = "Palabras", y = "Número de usos")
